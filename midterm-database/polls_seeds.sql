bad8456e718a0e0f40d8aa065d038f30d2a39157
SyedHamzaAsim <hamza.asim090@gmail.com>
2022-07-05 22:03:00 +0000

    seperated database  into multiple files

--- midterm-database/polls_seeds.sql
+++ midterm-database/polls_seeds.sql
@@ -1,15 +1,3 @@
-INSERT INTO users (name, email)
-VALUES ('Devin Sanders', 'tristanjacobs@gmail.com'),
-  ('Iva Harrison', 'allisonjackson@mail.com'),
-  ('Lloyd Jefferson', 'asherpoole@gmx.com'),
-  ('Dale Coleman', 'michaelgray@mail.com'),
-  ('Alejandro Osborne', 'ariaatkinson@outlook.com'),
-  ('Nell Medina', 'juliansantos@aol.com'),
-  ('Estelle Walsh', 'elistanton@yahoo.com'),
-  ('Herbert Graves', 'emilyowen@live.com'),
-  ('John Stevens', 'charliebattle@yahoo.com'),
-  ('Isabelle Robbins', 'miasutton@aol.com');
-  
 INSERT INTO polls (
     user_id,
     title,
@@ -80,19 +68,3 @@ VALUES (
     'www.google.com/links',
     'www.google.com/results'
   );
-
-
-INSERT INTO submission (poll_id, activity, rating, name)
-VALUES (1, 'Superman', 2, 'a'),
-  (1, 'Superman', 1, 'b'),
-  (1, 'Superman', 0, 'c'),
-  (1, 'Superman', 0, 'c'),
-  (1, 'Spiderman', 1, 'a'),
-  (1, 'Spiderman', 2, 'b'),
-  (1, 'Spiderman', 1, 'c'),
-  (1, 'Spiderman', 2, 'd'),
-  (1, 'Batman', 0, 'a'),
-  (1, 'Batman', 0, 'b'),
-  (1, 'Batman', 2, 'c'),
-  (1, 'Batman', 1, 'd');
-
---
+++ midterm-database/user_seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (name, email)
+VALUES ('Devin Sanders', 'tristanjacobs@gmail.com'),
+  ('Iva Harrison', 'allisonjackson@mail.com'),
+  ('Lloyd Jefferson', 'asherpoole@gmx.com'),
+  ('Dale Coleman', 'michaelgray@mail.com'),
+  ('Alejandro Osborne', 'ariaatkinson@outlook.com'),
+  ('Nell Medina', 'juliansantos@aol.com'),
+  ('Estelle Walsh', 'elistanton@yahoo.com'),
+  ('Herbert Graves', 'emilyowen@live.com'),
+  ('John Stevens', 'charliebattle@yahoo.com'),
+  ('Isabelle Robbins', 'miasutton@aol.com');
